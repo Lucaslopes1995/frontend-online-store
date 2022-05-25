270ce2064f4bf357c41e3786ac71d7a11d373d90
Lucas Soares Lopes <65666635+Lucaslopes1995@users.noreply.github.com>
2022-05-24 23:47:51 -0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,22 +17,22 @@ class Home extends React.Component {
     this.handleChangeRadio = this.handleChangeRadio.bind(this);
     this.setCategories = this.setCategories.bind(this);
   }
-  
+
   componentDidMount() {
     this.setCategories();
   }
-  
+
   handleChange({ target }) {
     const { name, value } = target;
     this.setState(() => ({ [name]: value }));
   }
-  
+
   handleChangeRadio(el) {
     this.setState({ selectedRadio: el.id }, async () => {
       this.handleButton();
     });
   }
-  
+
   handleButton = async () => {
     const { inputValue, selectedRadio } = this.state;
     if (inputValue !== '' || selectedRadio !== '') {
@@ -41,13 +41,13 @@ class Home extends React.Component {
       this.setState({ filteredProducts: productsByNameAndCategory });
     }
   }
-  
+
   async setCategories() {
     /* console.log(await getProductsByNameAndCategory('Agro', '')); */
     const categories = (await getCategories());
     this.setState({ listCategories: categories });
   }
-  
+
   addToCart = (product) => {
     const { cartProducts } = this.state;
     this.setState({ cartProducts: [...cartProducts, product] }, () => {
@@ -55,7 +55,7 @@ class Home extends React.Component {
       localStorage.setItem('productCart', JSON.stringify(Carrinho));
     });
   }
-  
+
   render() {
     const { inputValue, listProducts, listCategories, selectedRadio,
       filteredProducts } = this.state;
