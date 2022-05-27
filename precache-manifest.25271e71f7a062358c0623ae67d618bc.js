74bbf3d39b6130867e92419d9dd1d148fc5697e6
Lucaslopes1995 <Lucaslopes1995@users.noreply.github.com>
2022-05-27 18:59:08 +0000

    deploy: 0c53a73714cf497e31acb1591fa3fe3b2a3b91cc

---
+++ precache-manifest.25271e71f7a062358c0623ae67d618bc.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df1af400c49c6fd527a9824f7ae8fb23",
+    "url": "/frontend-online-store/index.html"
+  },
+  {
+    "revision": "491acd54a1df8b22af2d",
+    "url": "/frontend-online-store/static/css/main.239285e9.chunk.css"
+  },
+  {
+    "revision": "20660892978a207e6824",
+    "url": "/frontend-online-store/static/js/2.00a7fed6.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/frontend-online-store/static/js/2.00a7fed6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "491acd54a1df8b22af2d",
+    "url": "/frontend-online-store/static/js/main.40ee3647.chunk.js"
+  },
+  {
+    "revision": "582737c6385ae9ed11b6",
+    "url": "/frontend-online-store/static/js/runtime-main.720530ed.js"
+  },
+  {
+    "revision": "515fc584baa97d0aae930bc6161a864f",
+    "url": "/frontend-online-store/static/media/carrinho.515fc584.png"
+  },
+  {
+    "revision": "7171c5215cb41edb602026d7c8cf6740",
+    "url": "/frontend-online-store/static/media/estrela-vazia.7171c521.svg"
+  }
+]);
